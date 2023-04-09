34b997561ac39a9f6f5e9ec5db7111ce0e17f41c
Lias Tif <ltif427@gmail.com>
2023-04-09 19:32:02 +0300

    add person functionality

---
+++ TeamScheduleDataManager/Processors/AddingPersonProcessor.cs
@@ -0,0 +1,16 @@
+﻿using TeamScheduleApp.Models;
+
+namespace TeamScheduleDataManager.Processors
+{
+    public class AddingPersonProcessor
+    {
+        public void AddPerson(Person person)
+        {
+            using (var db = new TeamScheduleDbContext())
+            {
+                db.People.Add(person);
+                db.SaveChanges();
+            }
+        }
+    }
+}
